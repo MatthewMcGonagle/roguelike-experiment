5b45a920feb52deeccae260f036825ab52d7512e
Matthew McGonagle <mwmcgonagle@gmail.com>
2026-01-26 00:17:31 -0500

    Render squares from array.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,21 @@ use sdl3::pixels::Color;
 use sdl3::event::Event;
 use sdl3::keyboard::Keycode;
 use sdl3::rect::Rect;
+use sdl3::render::Canvas;
 use std::time::Duration;
+use sdl3::video::Window;
+
+fn draw_squares(coords: [(i32, i32); 4], canvas: &mut Canvas<Window>) { 
+    let s_width = 100;
+    // let square = Rect::new(300, 200, s_width, s_width);
+    let s_color = Color::RGB(125, 125, 125);
+
+    canvas.set_draw_color(s_color);
+    for &(x, y) in coords.iter() {
+        let square = Rect::new(x, y, s_width, s_width);
+        _ = canvas.fill_rect(square);
+    }
+}
 
 pub fn main() {
     let sdl_context = sdl3::init().unwrap();
@@ -23,10 +37,7 @@ pub fn main() {
 
     let mut event_pump = sdl_context.event_pump().unwrap();
     let mut i = 0;
-    let r_width = 200;
-    let r_height = 200;
-    let rect = Rect::new(300, 200, r_width, r_height);
-    let r_color = Color::RGB(125, 125, 125);
+    let coords = [(0, 0), (200, 300), (300, 500), (400, 400)];
 
     'running: loop {
         i = (i + 1) % 255;
@@ -42,9 +53,8 @@ pub fn main() {
             }
         }
         // The rest of the game loop goes here...
-       
-        canvas.set_draw_color(r_color);
-        _ = canvas.fill_rect(rect);
+      
+        draw_squares(coords, &mut canvas);
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
