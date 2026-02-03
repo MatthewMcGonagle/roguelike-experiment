f7b037303930cd78975a814769c51523f1a160d9
Matthew McGonagle <mwmcgonagle@gmail.com>
2026-02-02 19:21:56 -0500

    Switch to using vectors instead of arrays.

--- src/components.rs
+++ src/components.rs
@@ -4,7 +4,7 @@ pub struct Coordinates {
 }
 
 pub struct CoordinateComponents {
-    pub values: [Coordinates; 4],
+    pub values: Vec<Coordinates>,
 }
 
 pub struct Timer { pub time: u32, pub reset: u32 }
@@ -19,7 +19,7 @@ impl Timer {
 }
 
 pub struct ActionTimers {
-    pub values: [Timer; 4]
+    pub values: Vec<Timer>
 }
 
 impl ActionTimers {
@@ -39,10 +39,10 @@ impl Components {
     pub fn initialize() -> Components {
         Components {
             coords: CoordinateComponents {
-                values: [Coordinates{x: 0, y: 0}, Coordinates{x: 200, y: 300}, Coordinates{x: 300, y: 500}, Coordinates{x: 400, y: 400}],
+                values: Vec::from([Coordinates{x: 0, y: 0}, Coordinates{x: 200, y: 300}, Coordinates{x: 300, y: 500}, Coordinates{x: 400, y: 400}]),
             },
             action_timers: ActionTimers { 
-                values: [Timer { time: 10, reset: 5 }, Timer { time: 10, reset: 7 }, Timer { time: 13, reset: 13 }, Timer { time: 10, reset: 17}]
+                values: Vec::from([Timer { time: 10, reset: 5 }, Timer { time: 10, reset: 7 }, Timer { time: 13, reset: 13 }, Timer { time: 10, reset: 17}])
             }
         }
     }
