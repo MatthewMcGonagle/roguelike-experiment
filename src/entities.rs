391183ddca2c465bef51fb746eb78a7dcdefef32
Matthew McGonagle <mwmcgonagle@gmail.com>
2026-02-18 21:08:15 -0500

    Clear coordinates query on entity removal.

--- src/entities.rs
+++ src/entities.rs
@@ -78,7 +78,12 @@ impl Entities {
         e_components.component_types.values.get(e_id).map(
             |c_types| for c_type in c_types { 
                 match c_type {
-                    ComponentType::Coordinates => e_components.coords.values.remove(e_id), 
+                    ComponentType::Coordinates => {
+                        e_components.coords.values.get(e_id).map(|c|
+                            e_components.coords_query.get_mut(c.x, c.y).map(|s| *s = SpaceData::Empty)
+                        );
+                        e_components.coords.values.remove(e_id);
+                    },
                     ComponentType::Blocking => e_components.blocking.values.remove(e_id),
                     ComponentType::ActionTimer => e_components.action_timers.values.remove(e_id),
                     ComponentType::Ai => e_components.ais.values.remove(e_id),
