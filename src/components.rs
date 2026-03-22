5407b7b4c875f949a89286bd15a9e1f57e19f21d
Matthew McGonagle <mwmcgonagle@gmail.com>
2026-03-22 16:14:31 -0400

    Finish refactor to data submodule.

--- src/components.rs
+++ src/components.rs
@@ -4,7 +4,6 @@ pub mod for_entities;
 use containers::*;
 use crate::data::*;
 use for_entities::*;
-use sdl3::pixels::Color;
 
 const CAPACITY: usize = 10;
 
@@ -15,79 +14,6 @@ pub trait Component<'a, T> where T: 'a {
     fn remove(&mut self, e_id: usize);
     fn iter_w_eid(&'a self) -> impl Iterator<Item = (usize, &'a Option<T>)>;
     fn iter_mut_w_eid(&'a mut self) -> impl Iterator<Item = (usize, &'a mut Option<T>)>;
-}
-
-#[derive(Clone)]
-pub enum ComponentType {
-    ComponentTypeList,
-    Coordinates,
-    CoordinatesQuery,
-    DecisionTimer,
-    Ai,
-    State,
-    Render,
-    Target,
-    TargetedBy,
-    Blocking,
-    Alignment,
-    Health
-}
-
-#[derive(Clone)]
-pub struct Coordinates {
-    pub x: usize,
-    pub y: usize 
-}
-
-#[derive(Clone)]
-pub enum SpaceData {
-    Empty,
-    HasEid(usize)
-}
-
-#[derive(Clone, PartialEq)]
-pub enum BlockingType {
-    Movement
-}
-
-#[derive(Clone)]
-pub struct Timer { pub time: u32, pub reset: u32 }
-
-pub enum TimerResult {
-    Tick,
-    Reset
-}
-
-impl Timer {
-    pub fn update(&mut self) -> TimerResult {
-        self.time = self.time - 1;
-        if self.time <= 0 {
-            self.time = self.reset;
-            return TimerResult::Reset;
-        }
-        return TimerResult::Tick;
-    }
-}
-
-#[derive(Clone)]
-pub enum Ai {
-    ShiftX,
-    ShiftY,
-    AddAvailableSquare,
-    Kill,
-    User
-}
-
-#[derive(Clone)]
-pub struct Render {
-    pub color: Color
-}
-
-#[derive(Clone)]
-pub enum AlignmentType {
-    User,
-    Neutral,
-    HostileToUser
 }
 
 pub struct EntityComponents {
@@ -124,35 +50,6 @@ impl EntityComponents {
     }
 }
 
-pub struct Display {
-    pub width: u32,
-    pub height: u32,
-    pub coord_scale: usize
-}
-
-#[derive(PartialEq)]
-pub enum LoopState {
-    RunTimers,
-    MakeDecisions,
-    DoActions,
-    User(usize)
-}
-
-pub enum Direction {
-    Down,
-    Up,
-    Right,
-    Left
-}
-
-pub enum Action {
-    Move(usize, Direction),
-    Attack(usize, usize),
-    Spawn(usize),
-    Kill(usize),
-    Wait
-}
-
 pub struct PlannedActions {
     pub values: Vec<Action>
 }
@@ -161,10 +58,6 @@ impl PlannedActions {
     pub fn initialize(capacity: usize) -> PlannedActions {
         PlannedActions { values: Vec::with_capacity(capacity) }
     }
-}
-
-pub enum Reaction {
-    Kill(usize)
 }
 
 pub struct ReactionsReady {
--- src/data.rs
+++ src/data.rs
@@ -1,7 +1,115 @@
+use sdl3::pixels::Color;
+
 pub enum Errors {
     CoordinateMissing,
     MissingExpectedEid,
     SpaceAlreadyNonempty,
     UnexpectedlyEmpty,
     NotExpectingAiForUser
+}
+
+#[derive(Clone)]
+pub enum ComponentType {
+    ComponentTypeList,
+    Coordinates,
+    CoordinatesQuery,
+    DecisionTimer,
+    Ai,
+    State,
+    Render,
+    Target,
+    TargetedBy,
+    Blocking,
+    Alignment,
+    Health
+}
+
+#[derive(Clone)]
+pub struct Coordinates {
+    pub x: usize,
+    pub y: usize 
+}
+
+#[derive(Clone)]
+pub enum SpaceData {
+    Empty,
+    HasEid(usize)
+}
+
+#[derive(Clone, PartialEq)]
+pub enum BlockingType {
+    Movement
+}
+
+#[derive(Clone)]
+pub struct Timer { pub time: u32, pub reset: u32 }
+
+pub enum TimerResult {
+    Tick,
+    Reset
+}
+
+impl Timer {
+    pub fn update(&mut self) -> TimerResult {
+        self.time = self.time - 1;
+        if self.time <= 0 {
+            self.time = self.reset;
+            return TimerResult::Reset;
+        }
+        return TimerResult::Tick;
+    }
+}
+
+#[derive(Clone)]
+pub enum Ai {
+    ShiftX,
+    ShiftY,
+    AddAvailableSquare,
+    Kill,
+    User
+}
+
+#[derive(Clone)]
+pub struct Render {
+    pub color: Color
+}
+
+#[derive(Clone)]
+pub enum AlignmentType {
+    User,
+    Neutral,
+    HostileToUser
+}
+
+pub struct Display {
+    pub width: u32,
+    pub height: u32,
+    pub coord_scale: usize
+}
+
+#[derive(PartialEq)]
+pub enum LoopState {
+    RunTimers,
+    MakeDecisions,
+    DoActions,
+    User(usize)
+}
+
+pub enum Direction {
+    Down,
+    Up,
+    Right,
+    Left
+}
+
+pub enum Action {
+    Move(usize, Direction),
+    Attack(usize, usize),
+    Spawn(usize),
+    Kill(usize),
+    Wait
+}
+
+pub enum Reaction {
+    Kill(usize)
 }
