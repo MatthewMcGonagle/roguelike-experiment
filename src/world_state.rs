22ae9797f790f7bdd02002e97680700442b53e4a
Matthew McGonagle <mwmcgonagle@gmail.com>
2026-04-23 20:34:47 -0400

    First try to iterating over splits.

--- src/world_state.rs
+++ src/world_state.rs
@@ -1,12 +1,19 @@
-use data::*;
+use crate::data::*;
 
-fn parse_line(l: str) {
-    for (row_i, space) in s.split(' ').filter(|s| s != "").enumerate() {
-    }
+fn parse_line(row_i: usize, l: &str) -> Result<Vec<WorldState>, Errors> {
+    let states = l.split(' ').filter(|s| *s != "").enumerate().map(|(col_i, w)|
+        match w {
+            "#" => Ok(WorldState::Wall(col_i, row_i)),
+            _ => Err(Errors::UnknownWorldState(w.to_string())) 
+        }
+    );
+    states.collect()
 }
 
-pub fn parse_world_state(world_string: str) {
-    for (col_i, line) in world_string.split('\n').enumerate() {
-        parse_line(line);
-    }
+pub fn parse_world_state(world_string: &str) -> Result<Vec<WorldState>, Errors> {
+    let parsed_lines = world_string.split('\n').enumerate().map(|(row_i, line)|
+        parse_line(row_i, line)
+    );
+    let nested = parsed_lines.collect::<Result<Vec<_>, _>>()?;
+    Ok(nested.into_iter().flatten().collect())
 }
