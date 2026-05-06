1ab5de9f579e6461ecfb2806b5a55a18886964db
Matthew McGonagle <mwmcgonagle@gmail.com>
2026-05-06 18:26:56 -0400

    Better use of iterators.

--- src/world_state.rs
+++ src/world_state.rs
@@ -10,7 +10,7 @@ fn parse_line(row_i: usize, l: &str) -> Result<Vec<WorldState>, Errors> {
     states.collect()
 }
 
-pub fn parse_world_state(world_string: &str) -> Result<Vec<WorldState>, Errors> {
+pub fn parse_world_state<'a>(world_string: &'a str) -> Result<Vec<WorldState>, Errors> {
     let parse_state = |row_i, col_i, w| {
         match w {
             "#" => Ok(WorldState::Wall(col_i, row_i)),
@@ -18,9 +18,16 @@ pub fn parse_world_state(world_string: &str) -> Result<Vec<WorldState>, Errors> 
         }
     };
 
-    let world_states = world_string.split('\n').enumerate().map(
-        |(row_i, l)| l.split(' ').filter(|s| *s != "").enumerate().map(
-            move |(col_i, w)| (row_i, col_i, w))
-    ).flatten();
+    let parse_row = |(row_i, l): (_, &'a str)| {
+        l.split(' ')
+            .filter(|s| *s != "")
+            .enumerate()
+            .map(move |(col_i, w)| (row_i, col_i, w))
+    };
+
+    let split_rows = |ls: &'a str| ls.split('\n').enumerate();
+
+    let world_states = split_rows(world_string).map(parse_row).flatten();
+
     world_states.map(|(row_i, col_i, w)| parse_state(row_i, col_i, w)).collect() 
 }
