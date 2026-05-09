8591a7fde65496ccdcbe2e5bb1b05a6baf983b96
Matthew McGonagle <mwmcgonagle@gmail.com>
2026-05-08 20:46:01 -0400

    Better world state parsing.

--- src/world_state.rs
+++ src/world_state.rs
@@ -3,7 +3,8 @@ use crate::data::*;
 pub fn parse_world_state<'a>(world_string: &'a str) -> Result<Vec<WorldState>, Errors> {
     let parse_state = |row_i, col_i, w| {
         match w {
-            "#" => Ok(WorldState::Wall(col_i, row_i)),
+            "#" => Ok(Some(WorldState::Wall(col_i, row_i))),
+            "." => Ok(None),
             _ => Err(Errors::UnknownWorldState(w.to_string())) 
         }
     };
@@ -19,5 +20,10 @@ pub fn parse_world_state<'a>(world_string: &'a str) -> Result<Vec<WorldState>, E
 
     let world_states = split_rows(world_string).map(parse_row).flatten();
 
-    world_states.map(|(row_i, col_i, w)| parse_state(row_i, col_i, w)).collect() 
+    world_states.map(|(row_i, col_i, w)| parse_state(row_i, col_i, w))
+        .collect::<Result<Vec<_>, _>>()
+        .map(|maybes|
+            maybes.into_iter()
+                .flatten()
+                .collect::<Vec<WorldState>>())
 }
