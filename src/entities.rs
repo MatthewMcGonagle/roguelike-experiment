6cb73c6d60212f24ec2b394dceadbc3bff55ae8c
Matthew McGonagle <mwmcgonagle@gmail.com>
2026-07-23 17:59:16 -0400

    Only kill owner, self should be killed automatically.

--- src/entities.rs
+++ src/entities.rs
@@ -196,6 +196,7 @@ impl Entities {
                 .filter(|(_, id)| *id == e_id)
                 .map(|(i, _)| i)
                 .collect();
+
         // Make sure we only make the e_id free if we actually deactivated it.
         if let Some(i) = inds.get(0) {
             self.active_ids.swap_remove(*i);
