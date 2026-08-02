55f65908aea3fcde30469b6301d5f935b73ce892
Matthew McGonagle <mwmcgonagle@gmail.com>
2026-08-01 20:03:44 -0400

    Add tests for removing owner/owned entities.

--- src/entities.rs
+++ src/entities.rs
@@ -257,3 +257,80 @@ impl Entities {
         components.component_types.remove(e_id);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn remove_works_on_owner() {
+        let mut components = Components::initialize(2, 0, 0);
+        let mut entities = Entities::initialize(2);
+
+        let owner = entities.add_entity_buffer(
+            &mut components,
+            &EntityBuffer {
+                ai: None,
+                alignment: None,
+                blocking: None,
+                coords: None,
+                decision_timer: None,
+                health: None,
+                owner: None,
+                render: None,
+                state: None}).unwrap();
+
+        let owned = entities.add_entity_buffer(
+            &mut components,
+            &EntityBuffer {
+                ai: None,
+                alignment: None,
+                blocking: None,
+                coords: None,
+                decision_timer: None,
+                health: None,
+                owner: Some(owner),
+                render: None,
+                state: None}).unwrap();
+
+        entities.remove(owner, &mut components);
+
+        assert_eq!(4, 4);
+    }
+
+    #[test]
+    fn remove_works_on_owned() {
+        let mut components = Components::initialize(2, 0, 0);
+        let mut entities = Entities::initialize(2);
+
+        let owner = entities.add_entity_buffer(
+            &mut components,
+            &EntityBuffer {
+                ai: None,
+                alignment: None,
+                blocking: None,
+                coords: None,
+                decision_timer: None,
+                health: None,
+                owner: None,
+                render: None,
+                state: None}).unwrap();
+
+        let owned = entities.add_entity_buffer(
+            &mut components,
+            &EntityBuffer {
+                ai: None,
+                alignment: None,
+                blocking: None,
+                coords: None,
+                decision_timer: None,
+                health: None,
+                owner: Some(owner),
+                render: None,
+                state: None}).unwrap();
+
+        entities.remove(owned, &mut components);
+
+        assert_eq!(4, 4);
+    }
+}
